74d6d992f1bbeef5d0c2d4527212240be2655bec
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-10-05 21:24:11 +0300

    Fixing a problem with $scope values not being updated when <input> values are modified See: http://stackoverflow.com/questions/17606936/angularjs-dot-in-ng-model

--- src/main/web/app/scripts/controllers/mainController.js
+++ src/main/web/app/scripts/controllers/mainController.js
@@ -12,15 +12,17 @@ angular.module('BootstrapApplication.controllers')
                 'Karma'
             ];
 
-            $scope.lat = '52.232222';
-            $scope.lon = '21.008333';
+            $scope.coords = {
+                lat: '52.232222',
+                lon: '21.008333'
+            };
 
             $scope.alerts = [];
 
             $scope.city = '';
 
             $scope.findCity = function() {
-                CityService.findCity($scope.lat, $scope.lon, function(data) {
+                CityService.findCity($scope.coords.lat, $scope.coords.lon, function(data) {
                     $scope.city = data;
                 });
             };
